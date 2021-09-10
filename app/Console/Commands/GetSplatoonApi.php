65799bf83afd174c507a9c4562c81886b455a200
mingming <lauchunming0613@gmail.com>
2021-09-10 11:43:02 +0800

    Update API Data Get

--- app/Console/Commands/GetSplatoonApi.php
+++ app/Console/Commands/GetSplatoonApi.php
@@ -209,14 +209,14 @@ class GetSplatoonApi extends Command
 
         foreach($records->records->league_stats as $league_type => $medals_array ){
             foreach($medals_array as $medals_type => $medals_count){
-                if(PlayerMedalsStatus::getMedalsByType( $league_type , substr($medals_type,-6) )){
+                if(PlayerMedalsStatus::getMedalsByType( $league_type , substr($medals_type, 0, -6) )){
                     continue;
                 }   
 
                 $my_medals_records = new PlayerMedalsStatus();
                 $my_medals_records->player_id = $records->records->player->principal_id;
                 $my_medals_records->league_type = $league_type;
-                $my_medals_records->medals_type = substr($medals_type,-6);
+                $my_medals_records->medals_type = substr($medals_type, 0, -6);
                 $my_medals_records->medals_count = $medals_count;
                 $my_medals_records->save();
             }

73c044be5ae71c05dedd0c2cb49ddf1e8cf30265
mingming <lauchunming0613@gmail.com>
2021-12-10 20:48:47 +0800

    Update Gallery Controller (Bug Fix)

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -12,6 +12,6 @@ class GalleryController extends Controller
 {
     public function index(){
 		$data = Gallery::get();
-    	return view('gallery.index')->with(['data'=>$data]);
+    	return view('pages.gallery')->with(['data'=>$data]);
     }
 }

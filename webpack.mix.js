50b6d9385b25f097eca81bd3fd9cf28b4dd733e4
mingming <lauchunming0613@gmail.com>
2022-01-21 21:00:39 +0800

    File Fix on BxSlider Compile

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,16 +15,16 @@ const mix = require('laravel-mix');
 //     .postCss('resources/css/app.css', 'public/css', [
 //         //
 //     ]);
+mix.copyDirectory('resources/images', 'public/images');
+mix.copyDirectory('resources/fonts', 'public/fonts');
 mix.js('resources/js/jquery-3.6.0.min.js', 'public/js');
-mix.js('resources/js/jquery.bxslider.min.js', 'public/js')
-    .postCss('resources/css/jquery.bxslider.css', 'public/css', [
-        //
-    ]);
 mix.js('resources/js/bootstrap.bundle.min.js', 'public/js')
     .postCss('resources/css/bootstrap.min.css', 'public/css', [
         //
     ]);
-mix.copyDirectory('resources/images', 'public/images');
-mix.copyDirectory('resources/fonts', 'public/fonts');
+mix.js('resources/js/jquery.bxslider.min.js', 'public/js')
+    .postCss('resources/css/jquery.bxslider.css', 'public/css', [
+        //
+    ]);
 mix.js('resources/js/app.js', 'public/js');
 mix.sass('resources/css/sass/app.scss', 'public/css').version().minify('public/css/app.css');
